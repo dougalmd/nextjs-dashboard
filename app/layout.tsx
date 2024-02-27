b3e6e65ae16b8cade7c324312893c29eb335e74d
Douglz <douglas.almeida@goutec.com.br>
2024-02-27 16:34:02 -0300

    Next.js Dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import '@/app/ui/global.css'
+import { inter } from '@/app/ui/fonts'
+import { ReactNode } from 'react'
+
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="pt-br">
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
-  );
+  )
 }
